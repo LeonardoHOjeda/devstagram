71210a18344f1ab5d8b70717f51d6493e6a0676a
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2022-12-02 22:59:48 -0600

    Se agrega registro a la BD y redireccion al muro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,13 +17,27 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+
+      // Modificar el Request
+      $request->request->add(['username' => Str::slug($request->username)]);
+
       // Validacion
       $this->validate($request, [
         'name' => 'required|max:30',
         'username' => 'required|unique:users|min:3|max:20',
         'email' => 'required|unique:users|email|max:60',
-        'password' => 'required|min:8',
-        'password_confirmation' => '',
+        'password' => 'required|min:8|confirmed'
       ]);
+
+      // Insercion en la Base de Datos
+      User::create([
+        'name' => $request->name,
+        'username' => $request->username,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+      ]);
+
+      //Redireccionar
+      return redirect()->route('post.index');
     }
 }

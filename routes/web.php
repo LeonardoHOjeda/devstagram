6f07fdff7b4b62d38587b7063e9e72ab33019dfc
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2022-12-30 01:06:58 -0600

    Se visualizan las publicaciones en la pagina principal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
@@ -22,9 +24,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('principal');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
@@ -50,5 +50,9 @@ Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.sto
 // Like a las fotos
 Route::post('/post/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/post/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+// Siguiendo usuarios
+Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');
 
 
